c5fc4afc077816ce78eeaf4f519ea93e0dba1ecd
Lokesh R <31463924+lokesh-ctrl@users.noreply.github.com>
2024-04-14 08:21:48 +0530

    Update axios to set ngrok bypass header

--- react/react-products-project/src/services/product.js
+++ react/react-products-project/src/services/product.js
@@ -1,36 +1,60 @@
 import axios from "axios";
-const PRODUCTS_URL = "http://localhost:3000/api/products";
+const PRODUCTS_URL = "https://3e8a-117-221-56-127.ngrok-free.app/api/products";
 
 export const getProducts = async () => {
   // const response = await fetch("http://localhost:3000/products");
   //   const jsonData = await response.json();
   //   return jsonData;
 
-  const res = await axios.get(PRODUCTS_URL);
+  const res = await axios.get(PRODUCTS_URL, {
+    headers: {
+      "ngrok-skip-browser-warning": "69420",
+    },
+  });
   return res.data;
 };
 
 export const createProduct = async (name, category, price, stocked) => {
-  const res = await axios.post(PRODUCTS_URL, {
-    name,
-    category,
-    price: price,
-    stocked,
-  });
+  const res = await axios.post(
+    PRODUCTS_URL,
+    {
+      name,
+      category,
+      price: price,
+      stocked,
+    },
+    {
+      headers: {
+        "ngrok-skip-browser-warning": "69420",
+      },
+    }
+  );
   return res;
 };
 
 export const updateProduct = async (product) => {
-  const res = await axios.put(PRODUCTS_URL + "/" + product.id, {
-    name: product.name,
-    category: product.category,
-    price: product.price,
-    stocked: product.stocked,
-  });
+  const res = await axios.put(
+    PRODUCTS_URL + "/" + product.id,
+    {
+      name: product.name,
+      category: product.category,
+      price: product.price,
+      stocked: product.stocked,
+    },
+    {
+      headers: {
+        "ngrok-skip-browser-warning": "69420",
+      },
+    }
+  );
   return res;
 };
 
 export const deleteProduct = async (id) => {
-  const res = await axios.delete(PRODUCTS_URL + "/" + id);
+  const res = await axios.delete(PRODUCTS_URL + "/" + id, {
+    headers: {
+      "ngrok-skip-browser-warning": "69420",
+    },
+  });
   return res;
 };
