eedac1c97d8279470c9c0b8b7c29e3b4be1195cd
Lokesh R <31463924+lokesh-ctrl@users.noreply.github.com>
2024-07-02 15:19:15 +0530

    Add usecontext hook

--- react/react-hooks/src/App.jsx
+++ react/react-hooks/src/App.jsx
@@ -1,29 +1,88 @@
-import { useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import "./App.css";
+import { memo } from "react";
+
+const List = memo(() => {
+  const [list, setList] = useContext(ListContext);
+  return (
+    <div>
+      {list.map((element) => {
+        return (
+          <div>
+            {element}
+            <button
+              onClick={() => {
+                setList(list.filter((e) => e != element));
+              }}
+            >
+              Remove
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+});
+const ListContext = createContext(null);
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [list, setList] = useState([1, 2, 3, 4]);
 
-  const handleChange = () => {
-    setCount(count + 1);
-  };
+  function handleChange(event) {
+    setName(event.target.value);
+  }
 
-  useEffect(() => {
-    console.log("I'm running every time");
-  });
+  function addItem() {
+    console.log("coming here");
+    setList([...list, 1]);
+  }
+  // const [count, setCount] = useState(0);
+  // const [anotherCOunt, setAnotherCount] = useState(0);
+  // const [time, setTime] = useState(0);
 
+  // const handleChange = () => {
+  //   setCount(count + 1);
+  // };
+
+  // const handleAnotherChange = () => {
+  //   setAnotherCount(anotherCOunt + 1);
+  // };
+
+  // useEffect(() => {
+  //   console.log("in effect");
+  //   const interval = setInterval(() => setTime(time + 1), 1000);
+
+  //   return () => clearInterval(interval);
+  // }, [time]);
+
+  // useEffect(() => {
+  //   console.log("in APP");
+  // });
+
+  console.log("in app");
   return (
     <>
-      <div>{count}</div>
-      <button onClick={handleChange}>Click Me</button>
+      <ListContext.Provider value={[list, setList]}>
+        <List />
+        <button onClick={addItem}>Add item</button>
+      </ListContext.Provider>
     </>
   );
 }
 
 export default App;
 
+// Use Effect
+//  always
+//  mount
+//  update
+//  cleanup
 
-// always
-// mount
-// update
-// cleanup
+// Use memo
+
+// use context
+
+// use callback
+
+// use reducer
